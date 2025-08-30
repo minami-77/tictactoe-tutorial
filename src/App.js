6903ae7b57c25ca0d954ac7e0aee524937961ae0
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-08-30 21:22:58 +0900

    fix squares

--- src/App.js
+++ src/App.js
@@ -29,21 +29,21 @@ export default function Board() {
       <div className="board-row">
         {/* Give props to Square component (child) */}
         <Square
-          value={square[0]}
+          value={squares[0]}
           onSquareClick={() => handleClick(0)}
         />
-        <Square value={square[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={square[2]} onSquareClick={() => handleClick(2)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square value={square[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={square[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={square[5]} onSquareClick={() => handleClick(5)}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square value={square[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={square[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={square[8]} onSquareClick={() => handleClick(8)}/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
